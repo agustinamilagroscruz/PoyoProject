d636b7074e48feb0a6efb9572e9eda1829468af6
Agustina <agustinamcruz-2013@hotmail.com>
2025-01-08 00:06:20 -0300

    first commit

---
+++ src/poyoproyect/PoyoProyect.java
@@ -0,0 +1,22 @@
+package poyoproyect;
+
+public class PoyoProyect {
+
+    public static void main(String[] args) {
+        
+         //operacion de asignación 
+        int edad = 24;
+        double estatura = 1.57;
+        boolean tieneCarnetDeConducir= false;
+        char letra = 'A';
+        String direccion= "Azopardo 1778";
+        long numeroLargo= 342352452;
+        
+        System.out.println(" Hola Mi edad es: " + edad);
+        System.out.println("Mi estatura es : " + estatura);
+        System.out.println("Mi dirección es: " + direccion);
+        
+        
+    }
+    
+}
